6214537e255360fd8e9b88f1c98c2b74a377212d
m0000amir <mukhtarov.amir.a@gmail.com>
2022-10-24 13:54:33 +0300

    added Navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Navigation from './navigation';
 
 function App() {
   return (
     <div className="App">
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Navigation />
     </div>
   );
 }
---
+++ src/navigation/index.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "../components/Header";
+import Cart from "../pages/Cart";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Menu from "../pages/Menu";
+import PaymentSuccess from "../pages/PaymentSuccess";
+import Register from "../pages/Register";
+
+const Navigation = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} /> 
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/payment-success" element={<PaymentSuccess />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Navigation;
