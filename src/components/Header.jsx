c1daa4eac7ac0fea13555cb39e6b38af21ba2897
m0000amir <mukhtarov.amir.a@gmail.com>
2022-10-24 19:41:43 +0300

    added seed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,10 @@ import { Link } from "react-router-dom"
 const Header = () => {
   return (
     <nav id="header" className="bg-black text-white">
-      <div className="w-full container mx-auto flex flex-wrap items-center justify-content-between mt-0 py-2 text-white">
+      <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2 text-white">
         <div className="logo-wrapper pl-4 flex items-center">
           <Link to="/" className="toggleColor text-whtie no-underline hover:no-underline font-bold text-2xl lg:text-4xl">
-            <img src={foody} alt="logo" />
+            <img src={foody} alt="logo" className="w-40 h-40 object-cover"/>
           </Link>
         </div>
         <div className="nav-menu-wrapper flex items-center justify-between space-x-10">
